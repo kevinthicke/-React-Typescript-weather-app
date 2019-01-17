c1a0046a820ec9a20278404b2dd9d1489dfdce2a
Kevin Cabrera <keviincabrera@gmail.com>
2019-01-17 12:19:23 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Grid } from 'react-bootstrap';
+
+export default class App extends React.Component <{}, {}> {
+  render() {
+    return (
+      <Grid>
+        <h1>Weather App</h1>
+      </Grid>
+    );
+  }
+}
